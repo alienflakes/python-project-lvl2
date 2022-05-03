ae45c437c14e76d8dab2ae8cedbb5190dd6aebe7
phelix alienflakes <alienflakes38@gmail.com>
2022-05-03 18:52:52 +0300

    fix tests

---
+++ tests/fixtures/expected.py
@@ -0,0 +1,11 @@
+"""Expected results for gendiff tests."""
+
+
+expected_test_gendiff_flat_json = """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,7 @@
 """Testing gendiff."""
 
 from gendiff.gendiff import generate_diff
+from .fixtures import expected
 
 
 def test_gendiff():
@@ -8,11 +9,4 @@ def test_gendiff():
     assert generate_diff(
         'tests/fixtures/file1.json',
         'tests/fixtures/file2.json'
-    ) == """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+    ) == expected.expected_test_gendiff_flat_json
