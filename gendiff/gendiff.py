3791cb730f1508237c6896e10ca7a1a3187d6f71
phelix alienflakes <alienflakes38@gmail.com>
2022-05-07 18:42:10 +0300

    refactor visualize and jsonize_bool

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -18,19 +18,20 @@ def visualize(diffs):
         formatted string
 
     """
-    symbol_added = '+'
-    symbol_removed = '-'
-    symbol_same = ' '
+
+    symbols = {
+        'added': '+',
+        'removed': '-',
+        'same': ' ',
+        'changed_from_file1': '-',
+        'changed_from_file2': '+'
+    }
 
     lines = []
+
     for key, value in diffs.items():
-        for each in value:
-            name, data = each
-            symbol = symbol_same
-            if key == 'added' or key == 'changed_from_file2':
-                symbol = symbol_added
-            elif key == 'removed' or key == 'changed_from_file1':
-                symbol = symbol_removed
+        for name, data in value:
+            symbol = symbols[key]
             lines.append('  {0} {1}: {2}'.format(symbol, name, data))
 
     first_letter_index = 4
@@ -39,25 +40,9 @@ def visualize(diffs):
     return '{0}\n{1}\n{2}'.format('{', '\n'.join(lines), '}')
 
 
-def lowercase_bool(item):
-    """
-    Change Python bool to json-style lowercase string,
-    if item is boolean type. If not, change nothing.
-
-    Args:
-        item: a value from the diff dictionary
-
-    Returns:
-        lowercase string of corresponding bool value
-        or the same item if it's not bool.
-
-    """
-    if item is True:
-        return 'true'
-    if item is False:
-        return 'false'
-    if item is None:
-        return 'none'
+def jsonize_bool(item):
+    if type(item) is bool or None:
+        return json.dumps(item)
     return item
 
 
@@ -92,18 +77,18 @@ def generate_diff(file_path1, file_path2):
     added_keys = list(data_dct2.keys() - data_dct1.keys())
     removed_keys = list(data_dct1.keys() - data_dct2.keys())
     all_diffs['added'] = [
-        [key, lowercase_bool(data_dct2[key])]
+        [key, jsonize_bool(data_dct2[key])]
         for key in added_keys
     ]
     all_diffs['removed'] = [
-        [key, lowercase_bool(data_dct1[key])]
+        [key, jsonize_bool(data_dct1[key])]
         for key in removed_keys
     ]
 
     both = list(data_dct1.keys() & data_dct2.keys())
     for key in both:
-        value1 = lowercase_bool(data_dct1[key])
-        value2 = lowercase_bool(data_dct2[key])
+        value1 = jsonize_bool(data_dct1[key])
+        value2 = jsonize_bool(data_dct2[key])
         if value1 == value2:
             all_diffs['same'].append(
                 [key, value1]
