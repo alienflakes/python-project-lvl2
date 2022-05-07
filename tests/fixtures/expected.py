ab552833fea8832ac45b4ac41deaf5632f565919
phelix alienflakes <alienflakes38@gmail.com>
2022-05-07 17:45:09 +0300

    add tests

--- tests/fixtures/expected.py
+++ tests/fixtures/expected.py
@@ -1,11 +1,28 @@
 """Expected results for gendiff tests."""
 
 
-expected_flat_json = """{
+result_json_flat = """{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
+}"""
+
+result_json_empty = """{
+
+}"""
+
+result_json_add_all = """{
+  + host: hexlet.io
+  + timeout: 20
+  + verbose: true
+}"""
+
+result_json_remove_all = """{
+  - follow: false
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
 }"""
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,12 +1,23 @@
 """Testing gendiff."""
 
-from gendiff.gendiff import generate_diff
+from gendiff import generate_diff
 from .fixtures import expected
 
 
-def test_gendiff():
-
+def test_generate_diff():
     assert generate_diff(
         'tests/fixtures/file1.json',
         'tests/fixtures/file2.json'
-    ) == expected.expected_flat_json
+    ) == expected.result_json_flat
+    assert generate_diff(
+        'tests/fixtures/empty_file1.json',
+        'tests/fixtures/empty_file2.json'
+    ) == expected.result_json_empty
+    assert generate_diff(
+        'tests/fixtures/empty_file1.json',
+        'tests/fixtures/file2.json'
+    ) == expected.result_json_add_all
+    assert generate_diff(
+        'tests/fixtures/file1.json',
+        'tests/fixtures/empty_file2.json'
+    ) == expected.result_json_remove_all
