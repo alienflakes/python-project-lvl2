132b6247e5c6aa9b990b20e8c7ae3d964ec96803
phelix alienflakes <alienflakes38@gmail.com>
2022-05-08 19:43:07 +0300

    fix tests

--- tests/fixtures/expected.py
+++ tests/fixtures/expected.py
@@ -1,28 +1,11 @@
 """Expected results for gendiff tests."""
 
 
-result_json_flat = """{
+result_flat = """{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}"""
-
-result_json_empty = """{
-
-}"""
-
-result_json_add_all = """{
-  + host: hexlet.io
-  + timeout: 20
-  + verbose: true
-}"""
-
-result_json_remove_all = """{
-  - follow: false
-  - host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
 }"""
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -8,16 +8,4 @@ def test_generate_diff():
     assert generate_diff(
         'tests/fixtures/file1.json',
         'tests/fixtures/file2.json'
-    ) == expected.result_json_flat
-    assert generate_diff(
-        'tests/fixtures/empty_file1.json',
-        'tests/fixtures/empty_file2.json'
-    ) == expected.result_json_empty
-    assert generate_diff(
-        'tests/fixtures/empty_file1.json',
-        'tests/fixtures/file2.json'
-    ) == expected.result_json_add_all
-    assert generate_diff(
-        'tests/fixtures/file1.json',
-        'tests/fixtures/empty_file2.json'
-    ) == expected.result_json_remove_all
+    ) == expected.result_flat
