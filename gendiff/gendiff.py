048e54234e9df61597e6f996cbdff35b072c7b7d
phelix alienflakes <alienflakes38@gmail.com>
2022-05-08 19:36:41 +0300

    refactor gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,18 +1,20 @@
 """Compare files and generate diff.."""
 import json
-from os.path import abspath, isabs
+import os
+
+
+def jsonize_bool(item):
+    if type(item) is bool or None:
+        return json.dumps(item)
+    return item
 
 
 def visualize(diffs):
     """
     Construct a visual representation of files' diff.
-    1. Creates lists of data pairs and their status symbol.
-    2. Sorts lists by name of the pair.
-    3. Sets up formatted lines.
-    4. Constructs the visual output.
 
     Args:
-        diffs: dict of diff with key=status
+        diffs: dict
 
     Returns:
         formatted string
@@ -32,18 +34,14 @@ def visualize(diffs):
     for key, value in diffs.items():
         for name, data in value:
             symbol = symbols[key]
-            lines.append('  {0} {1}: {2}'.format(symbol, name, data))
+            lines.append(
+                '  {0} {1}: {2}'.format(symbol, name, jsonize_bool(data))
+            )
 
     first_letter_index = 4
     lines.sort(key=lambda line: line[first_letter_index])
 
     return '{0}\n{1}\n{2}'.format('{', '\n'.join(lines), '}')
-
-
-def jsonize_bool(item):
-    if type(item) is bool or None:
-        return json.dumps(item)
-    return item
 
 
 def generate_diff(file_path1, file_path2):
@@ -58,10 +56,10 @@ def generate_diff(file_path1, file_path2):
         formatted string of diff
 
     """
-    if not isabs(file_path1):
-        file_path1 = abspath(file_path1)
-    if not isabs(file_path2):
-        file_path2 = abspath(file_path2)
+    if not os.path.isabs(file_path1):
+        file_path1 = os.path.abspath(file_path1)
+    if not os.path.isabs(file_path2):
+        file_path2 = os.path.abspath(file_path2)
 
     data_dct1 = json.load(open(file_path1))
     data_dct2 = json.load(open(file_path2))
@@ -77,18 +75,18 @@ def generate_diff(file_path1, file_path2):
     added_keys = list(data_dct2.keys() - data_dct1.keys())
     removed_keys = list(data_dct1.keys() - data_dct2.keys())
     all_diffs['added'] = [
-        [key, jsonize_bool(data_dct2[key])]
+        [key, data_dct2[key]]
         for key in added_keys
     ]
     all_diffs['removed'] = [
-        [key, jsonize_bool(data_dct1[key])]
+        [key, data_dct1[key]]
         for key in removed_keys
     ]
 
     both = list(data_dct1.keys() & data_dct2.keys())
     for key in both:
-        value1 = jsonize_bool(data_dct1[key])
-        value2 = jsonize_bool(data_dct2[key])
+        value1 = data_dct1[key]
+        value2 = data_dct2[key]
         if value1 == value2:
             all_diffs['same'].append(
                 [key, value1]
