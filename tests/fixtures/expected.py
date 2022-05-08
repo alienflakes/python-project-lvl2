a8f90777c0c8665a0dba64bee5e53165597a3f67
phelix alienflakes <alienflakes38@gmail.com>
2022-05-08 19:57:43 +0300

    fix tests 2

--- tests/fixtures/expected.py
+++ tests/fixtures/expected.py
@@ -9,3 +9,23 @@ result_flat = """{
   + timeout: 20
   + verbose: true
 }"""
+
+result_add_all = """{
+  + follow: false
+  + host: hexlet.io
+  + proxy: 123.234.53.22
+  + timeout: 50
+}"""
+
+result_remove_all = """{
+  - follow: false
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+}"""
+
+result_same = """{
+    host: hexlet.io
+    timeout: 20
+    verbose: true
+}"""
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,6 +6,18 @@ from .fixtures import expected
 
 def test_generate_diff():
     assert generate_diff(
+        'tests/fixtures/empty.json',
+        'tests/fixtures/file1.json'
+    ) == expected.result_add_all
+    assert generate_diff(
+        'tests/fixtures/file1.json',
+        'tests/fixtures/empty.json'
+    ) == expected.result_remove_all
+    assert generate_diff(
+        'tests/fixtures/file2.json',
+        'tests/fixtures/file2.json'
+    )
+    assert generate_diff(
         'tests/fixtures/file1.json',
         'tests/fixtures/file2.json'
     ) == expected.result_flat
