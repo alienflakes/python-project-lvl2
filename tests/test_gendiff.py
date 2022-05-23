d8cb7e8941251056a0382f3f68568bb080d695e3
phelix alienflakes <alienflakes38@gmail.com>
2022-05-23 22:41:06 +0300

    add tests and fixtures for NESTED gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,15 +4,29 @@ from gendiff import generate_diff
 from .fixtures import expected
 
 
-def test_generate_diff_json():
+def test_generate_diff_json_flat_stylish():
     assert generate_diff(
-        'tests/fixtures/file1.json',
-        'tests/fixtures/file2.json'
-    ) == expected.result_flat
+        'tests/fixtures/file1_flat.json',
+        'tests/fixtures/file2_flat.json'
+    ) == expected.result_flat_stylish
 
 
-def test_generate_diff_yaml():
+def test_generate_diff_yaml_flat_stylish():
     assert generate_diff(
-        'tests/fixtures/file1.yaml',
-        'tests/fixtures/file2.yml'
-    ) == expected.result_flat
+        'tests/fixtures/file1_flat.yaml',
+        'tests/fixtures/file2_flat.yml'
+    ) == expected.result_flat_stylish
+
+
+def test_generate_diff_json_nested_stylish():
+    assert generate_diff(
+        'tests/fixtures/file1_nested.json',
+        'tests/fixtures/file2_nested.json'
+    ) == expected.result_nested_stylish
+
+
+def test_generate_diff_yaml_nested_stylish():
+    assert generate_diff(
+        'tests/fixtures/file1_nested.yaml',
+        'tests/fixtures/file2_nested.yml'
+    ) == expected.result_nested_stylish
