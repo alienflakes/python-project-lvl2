a8251909ce8f84816384ffe09fe61fcb05c9202f
phelix alienflakes <alienflakes38@gmail.com>
2022-10-18 16:42:37 +0300

    refactor (flatten if/else statements)

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -11,13 +11,13 @@ def make_template(
 
 def build_diff(dict1: dict, dict2: dict) -> dict:
     """Construct a tree of differences between two dicts."""
-
     all_diff = {}
 
     added_keys = list(dict2.keys() - dict1.keys())
     removed_keys = list(dict1.keys() - dict2.keys())
 
     for key in dict1.keys() | dict2.keys():
+
         if key in added_keys:
             all_diff.update(
                 {
@@ -32,31 +32,30 @@ def build_diff(dict1: dict, dict2: dict) -> dict:
                 }
             )
             continue
+
+        data1 = dict1[key]
+        data2 = dict2[key]
+        if isinstance(data1, dict) and isinstance(data2, dict):
+            all_diff.update(
+                {
+                    key: make_template(node_type='nested',
+                                       value=None,
+                                       children=build_diff(data1, data2))
+                }
+            )
+        elif data1 == data2:
+            all_diff.update(
+                {
+                    key: make_template(node_type='same', value=data1)
+                }
+            )
         else:
-            data1 = dict1[key]
-            data2 = dict2[key]
-            if isinstance(data1, dict) and isinstance(data2, dict):
-                all_diff.update(
-                    {
-                        key: make_template(node_type='nested',
-                                           value=None,
-                                           children=build_diff(data1, data2))
-                    }
-                )
-            else:
-                if data1 == data2:
-                    all_diff.update(
-                        {
-                            key: make_template(node_type='same', value=data1)
-                        }
-                    )
-                else:
-                    all_diff.update(
-                        {
-                            key: make_template(node_type='updated',
-                                               value=data1,
-                                               changed_value=data2)
-                        }
-                    )
+            all_diff.update(
+                {
+                    key: make_template(node_type='updated',
+                                       value=data1,
+                                       changed_value=data2)
+                }
+            )
 
     return dict(sorted(all_diff.items()))
